e15988c63744975408eb67ab9e0daedd1d5daa93
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-06-03 02:20:42 +0900

    😎한혜성_알고리즘문제풀이

--- HHS/220602_한혜성_전화번호목록.java
+++ HHS/220602_한혜성_전화번호목록.java
@@ -6,12 +6,14 @@ import java.io.*;
 class Solution {
     public boolean solution(String[] phone_book){
         boolean answer = true;
-        
+        // 전화번호 정렬한 후 
+        // 순서대로 돌면서 자신의 뒤만 체크
         Arrays.sort(phone_book);
         
         for(int i = 0; i < phone_book.length-1; i++){
             String num = phone_book[i];
             String num2 = phone_book[i+1];
+            //자신보다 길이가 더 길고 자신으로 시작하는 숫자이면 answer 변경후 break
             if(num.length() < num2.length() && num2.startsWith(num)){
                 answer = false;
                 break;
