104a7986e96ee9414ee01d8a07f421408f2c44ab
sowonwow2 <sowonwow2@gmail.com>
2022-08-11 15:21:53 +0900

    💪 최소원_알고리즘문제풀이

--- CSW/220810_최소원_프로그래머스3차모의고사_2.js
+++ CSW/220810_최소원_프로그래머스3차모의고사_2.js
@@ -1,41 +1,22 @@
 function solution(ingredient) {
     var answer = 0;
     
-    // 시간초과(82.4점)
-    // let arr = ingredient.join('');
-   
-    // while(1){
-    //     let idx = arr.indexOf('1231');
-    //     if(idx === -1) break;
-    //     arr = arr.substring(0,idx) + arr.substring(idx+4);
-    //     answer++;
-    // }
-
-    // 시간초과 (52.9점)
-    // let arr = ingredient.join('');
-    // let idx = arr.indexOf('1231');
-    // if(idx === -1) return 0;
-    // let stack = [];
-    // while(1){
-    //     if(stack.length>3){
-    //         let fourth = stack.pop();
-    //         let third = stack.pop();
-    //         let second = stack.pop();
-    //         let first = stack.pop();
-    //         if(first === 1 && second === 2&& third === 3 && fourth === 1){
-    //             answer++;
-    //             if(!ingredient.length) break;
-    //         }else{
-    //             stack.push(first, second, third, fourth);
-    //             if(!ingredient.length) break;
-    //             stack.push(ingredient.shift());
-    //         }
-    //     }else{
-    //         if(!ingredient.length) break;
-    //         stack.push(ingredient.shift());
-    //     }
-    // }
-
+    function solution(ingredient) {
+        var answer = 0;
+        let now = 0;
+        let len = ingredient.length;
+    
+        while(now<len-3){
+            if(ingredient[now]===1 && ingredient[now+1]===2 && ingredient[now+2]===3 && ingredient[now+3]===1){
+                answer++;
+                ingredient.splice(now, 4);
+                now = now-4 <0 ? 0 : now-4;
+            }
+            else now++;
+        }
+    
+        return answer;
+    }
 
     return answer;
 }
