54ef78bf2d8a68fd854833bda22368cf2c620be6
sowonwow2 <sowonwow2@gmail.com>
2022-08-29 22:34:10 +0900

    💪 최소원_알고리즘문제풀이

---
+++ CSW/220829_최소원_올바른괄호.js
@@ -0,0 +1,13 @@
+function solution(s){
+    var answer = true;
+
+    let cnt = 0;
+    for(let i=0; i<s.length; i++){
+        if(s[i] === '(') cnt++;
+        else cnt--;
+        if(cnt<0) return false;
+    }
+    if(cnt>0) return false;
+    
+    return answer;
+}
