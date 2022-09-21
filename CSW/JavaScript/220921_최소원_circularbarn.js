b6a8255f8d915a922d27cd41d5a13c37fcc52703
sowonwow2 <sowonwow2@gmail.com>
2022-09-21 19:11:15 +0900

    💪 최소원_알고리즘문제풀이

--- CSW/JavaScript/220921_최소원_circularbarn.js
+++ CSW/JavaScript/220921_최소원_circularbarn.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var input = fs.readFileSync(`C://Users/SONG/Desktop/pjt/Algorithm_Study_Jeans/CSW/JavaScript/example.txt`).toString().trim().split('\n');
+var input = fs.readFileSync(`/dev/stdin`).toString().trim().split('\n');
 
 let N = parseInt(input[0]);
 let energy = 0;
@@ -32,11 +32,10 @@ function isCow(blankP){
 
 
 
-while(barn.some(e=>e>=2)){
+while(barn.some(e=>!e)){
     // 반시계방향으로 빈칸이 있는지 검사
     for(let i=N-1; i>=0; i--){
-        let moreTwo = barn.some(e=>e>=2);
-        if(!barn[i] && moreTwo){
+        if(!barn[i]){
             isCow(i);
         }
     }
